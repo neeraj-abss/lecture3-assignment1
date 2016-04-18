673ba49a04ad95e425428505eb9659bb17f8ad34
MishraN <mishran@advisory.com>
2016-04-18 16:28:19 +0530

    sqrtt and cuberoot program

---
+++ assignment1.java
@@ -0,0 +1,15 @@
+ import java.lang.Math;
+
+public class assignment1 {
+
+    public static void main(String[] args) {
+        double cube = 125.0d;
+        double square = 100.0d;
+        double cbrt = Math.cbrt(cube);
+        System.out.println("Cube root of " + cube + " is " + cbrt);
+
+        double sqrt = Math.sqrt(square);
+        System.out.println("Square root of " + square + " is " + sqrt);
+    }
+
+}
